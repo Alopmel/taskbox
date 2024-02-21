47bc2f86797ed986ad1223edb940fbfbb65347d0
Alba Blanca Lopez Melian <alba.lopezmelian@plexus.tech>
2024-02-21 14:38:27 +0100

    primer componente task terminado

--- storybook-tutorial/taskbox/.storybook/main.js
+++ storybook-tutorial/taskbox/.storybook/main.js
@@ -1,11 +1,15 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+   stories: [
+       '../src/**/*.stories.mdx',
+       '../src/**/*.stories.@(js|jsx|ts|tsx)'
+     ],
   staticDirs: ["../public"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
   ],
   framework: {
     name: "@storybook/react-vite",
@@ -15,4 +19,5 @@ const config = {
     autodocs: "tag",
   },
 };
+
 export default config;
