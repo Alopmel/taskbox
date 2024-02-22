3b9822fc239ccc1bc701df4f463a53ced5be35ef
Alba Blanca Lopez Melian <alba.lopezmelian@plexus.tech>
2024-02-22 09:20:11 +0100

    Added Github Actions

--- storybook-tutorial/taskbox/.storybook/main.js
+++ storybook-tutorial/taskbox/.storybook/main.js
@@ -1,15 +1,13 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-   stories: [
-       '../src/**/*.stories.mdx',
-       '../src/**/*.stories.@(js|jsx|ts|tsx)'
-     ],
+  stories: ["../src/components/**/*.stories.@(js|jsx)"],
   staticDirs: ["../public"],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-a11y",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
   ],
   framework: {
     name: "@storybook/react-vite",
